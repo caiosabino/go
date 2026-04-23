ec3763894f7f30bf6d440a05aa2717f5ba91ef1d
caiosabino <caio.cassunde@gmail.com>
2026-04-22 21:24:26 -0300

    ponteiros

--- main.go
+++ main.go
@@ -3,5 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("oi")
+	a := 10
+
+	p := &a
+	pp := &p
+
+	fmt.Println(a)
+	fmt.Printf("%T\n", a)
+	fmt.Println(*p)
+	fmt.Println(p)
+	fmt.Printf("%T\n", p)
+	fmt.Println(**pp)
+	fmt.Println(pp)
+	fmt.Printf("%T\n", pp)
 }
